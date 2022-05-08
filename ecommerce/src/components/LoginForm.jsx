428c15877b3a9c67e0d01b1dd95e08567d885bc3
ehsanmiran <92309474+ehsanmiran@users.noreply.github.com>
2022-05-08 02:07:14 +0200

    Register Login forms are complete.. db to come

--- ecommerce/src/components/LoginForm.jsx
+++ ecommerce/src/components/LoginForm.jsx
@@ -1,32 +1,46 @@
-import React from 'react';
+import { useState } from 'react';
 import loginImg from '../img/avatar.png'
 import './style.scss'
 
-export default class LoginView extends React.Component {
 
-  render() {
-    return (
+const LoginForm = () => {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+  
+  const onChange = e => {
+    setFormData(state => ({
+      ...state,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  return (
     <div className="base-container">
+      <div className="image">
+        <img src={loginImg} alt="" />
+      </div>
       <div className="header">Login</div>
       <div className="content">
-        <div className="image">
-          <img src={loginImg} alt="" />
-        </div>
         <div className="form">
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" placeholder="username"/>
+        <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input value={formData.email} onChange={onChange} type="email" name="email" placeholder="email"/>
           </div>
           <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" placeholder="password"/>
+            <label htmlFor="password">Password:</label>
+            <input value={formData.password} onChange={onChange} type="password" name="password" placeholder="password"/>
           </div>
         </div>
+        <p>Not registerd? <a href="/register"  className='link'> Register here!</a></p>
       </div>
       <div className="footer">
         <button type="button" className="btn">Login</button>
       </div>
     </div>
-    );
-  }
+  );
 }
+
+export default LoginForm
--- ecommerce/src/components/RegisterForm.jsx
+++ ecommerce/src/components/RegisterForm.jsx
@@ -1,37 +1,51 @@
-import React from 'react';
+import { useState } from 'react';
 import loginImg from '../img/avatar.png'
 import './style.scss'
 
 
-export default class RegisterView extends React.Component {
+const RegisterForm = () => {
 
-  render() {
-    return (
+  const [formData, setFormData] = useState({
+    userName: '',
+    email: '',
+    password: ''
+  })
+  
+  const onChange = e => {
+    setFormData(state => ({
+      ...state,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  return (
     <div className="base-container">
+      <div className="image">
+        <img src={loginImg} alt="" />
+      </div>
       <div className="header">Register</div>
       <div className="content">
-        <div className="image">
-          <img src={loginImg} alt="" />
-        </div>
         <div className="form">
           <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" placeholder="username"/>
+            <label htmlFor="userName">Full name:</label>
+            <input value={formData.userName} onChange={onChange} type="text" id="userName" name="userName" placeholder="full name"/>
           </div>
           <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" placeholder="email"/>
+            <label htmlFor="email">Email:</label>
+            <input value={formData.email} onChange={onChange} type="email" name="email" placeholder="email"/>
           </div>
           <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" placeholder="password"/>
+            <label htmlFor="password">Password:</label>
+            <input value={formData.password} onChange={onChange} type="password" name="password" placeholder="password"/>
           </div>
         </div>
+        <p>Already registerd? <a href="/login"  className='link'> Login here!</a></p>
       </div>
       <div className="footer">
         <button type="button" className="btn">Register</button>
       </div>
     </div>
-    );
-  }
+  );
 }
+
+export default RegisterForm
